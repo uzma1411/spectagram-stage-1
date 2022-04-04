bfa56f7f9105f632cc2b954212a8bcf008f23834
uzma1411 <47413610+uzma1411@users.noreply.github.com>
2022-04-04 14:56:41 +0530

    Update DrawerNavigator.js

--- navigation/DrawerNavigator.js
+++ navigation/DrawerNavigator.js
@@ -1,1 +1,15 @@
-// Write the code for Drawer Navigator
+
+import React from "react";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import TabNavigator from "./TabNavigator";
+import Profile from "../screens/Profile";
+
+const Drawer = createDrawerNavigator();
+
+const DrawerNavigator = () => {
+    return (
+      // Write the code for Drawer Navigator here
+    );
+};
+
+export default DrawerNavigator;
